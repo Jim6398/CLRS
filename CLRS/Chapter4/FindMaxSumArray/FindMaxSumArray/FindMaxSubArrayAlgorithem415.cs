e6021d208c198ec2fbac627678178bd7c6f80e9f
Homer Guo <guochao6398@126.com>
2019-10-27 20:06:41 +0800

    Chapter4.1

---
+++ CLRS/Chapter4/FindMaxSumArray/FindMaxSumArray/FindMaxSubArrayAlgorithem415.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMaxSumArray
+{
+    public class FindMaxSubArrayAlgorithem415
+    {
+        public FindMaxSubArrayAlgorithem415(int[] inputArray)
+        {
+            this.inputArray = inputArray;
+        }
+
+        private int[] inputArray;
+
+        /// <summary>
+        /// Solutions for  4.1-5
+        /// </summary>
+        /// <returns></returns>
+        public SubArrayProp FindMaximumSubArray()
+        {
+            int lastMaxSum = int.MinValue;
+            SubArrayProp lastMaxSubPop = null;
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                int sum = 0;
+                for (int j = i; j >= 0; j--)
+                {
+                    sum += inputArray[j];
+                    if (sum >= lastMaxSum)
+                    {
+                        lastMaxSubPop = new SubArrayProp { left = j, right = i, value = sum };
+                        lastMaxSum = sum;
+                    }
+                }
+            }
+
+            return lastMaxSubPop;
+        }
+    }
+}
--- CLRS/Chapter4/FindMaxSumArray/FindMaxSumArray/Program.cs
+++ CLRS/Chapter4/FindMaxSumArray/FindMaxSumArray/Program.cs
@@ -5,18 +5,31 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace FindMaxSumArray
-{h
+{
     class Program
     {
         public static void Main(string[] args)
         {
+            Console.WriteLine("BEGIN 4.1.3");
             int[] inputArray = new int[] { 13, -3, -25, 20, -3, -16, -23, 18, 20, -7, 12, -5, -22, 15, -4, 7 };
             FindMaxSubArrayAlgorithem findMaxArray = new FindMaxSubArrayAlgorithem(inputArray);
 
             SubArrayProp prop = findMaxArray.FindMaximumSubArray(0, inputArray.Length - 1);
 
             Console.WriteLine(prop.value);
-            for (int i= prop.left; i<= prop.right;i++)
+            for (int i = prop.left; i <= prop.right; i++)
+            {
+                Console.Write(inputArray[i] + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("BEGIN 4.1.5");
+
+            // Test Algorithem for 4.1.5
+            FindMaxSubArrayAlgorithem415 findMaxArray415 = new FindMaxSubArrayAlgorithem415(inputArray);
+            SubArrayProp prop415 = findMaxArray415.FindMaximumSubArray();
+
+            Console.WriteLine(prop415.value);
+            for (int i = prop415.left; i <= prop415.right; i++)
             {
                 Console.Write(inputArray[i] + " ");
             }
