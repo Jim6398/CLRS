e0e4f524f8693824045001e19ba6ce1e4af9bb0b
Homer Guo <guochao6398@126.com>
2020-06-14 23:03:34 +0800

    The first quickSort Example from book Chapter7.

---
+++ CLRS/Chapter7/QuickSort7.1/QuickSortOriginalTest/QuickSortOriginalTest/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace QuickSortOriginalTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 13,19,9,5,12,8,7,4,21,2,6,11 };
+            QuickSort(array, 0, array.Length - 1);
+            Console.WriteLine(string.Join(',', array));
+        }
+
+        public static void QuickSort(int[] arrayToSort, int startIndex, int endIndex)
+        {
+            if (startIndex >= endIndex)
+            {
+                return;
+            }
+            int middleIndex = Partition(arrayToSort, startIndex, endIndex);
+            QuickSort(arrayToSort, startIndex, middleIndex - 1);
+            QuickSort(arrayToSort, middleIndex + 1, endIndex);
+
+        }
+
+        private static int Partition(int[] arrayToSort, int startIndex, int endIndex)
+        {
+            int i = startIndex - 1;
+            int x = arrayToSort[endIndex];
+            for (int j = startIndex; j < endIndex; j++)
+            {
+                if (arrayToSort[j] <= x)
+                {
+                    Exchange(ref arrayToSort[j], ref arrayToSort[++i]);
+                }
+            }
+
+            Exchange(ref arrayToSort[i + 1], ref arrayToSort[endIndex]);
+            return i + 1;
+        }
+
+        private static void Exchange(ref int left, ref int right)
+        {
+            int temp = left;
+            left = right;
+            right = temp;
+        }
+    }
+}
