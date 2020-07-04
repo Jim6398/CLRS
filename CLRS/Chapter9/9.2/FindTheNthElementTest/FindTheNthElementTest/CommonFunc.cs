3eeb711ad3b9023a5e1f269273525266096c9027
Homer Guo <guochao6398@126.com>
2020-07-04 17:40:42 +0800

    Randomized-Select Implemented by C#

---
+++ CLRS/Chapter9/9.2/FindTheNthElementTest/FindTheNthElementTest/CommonFunc.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace FindTheNthElementTest
+{
+
+    public class RandowWithNoDuplication
+    {
+
+        private HashSet<int> existingSet = new HashSet<int>();
+        private int from, to;
+        public RandowWithNoDuplication(int from, int end)
+        {
+            this.from = from;
+            this.to = end;
+        }
+
+        public int GetRandomValue()
+        {
+            Random r = new Random();
+            int value = 0;
+            do
+            {
+                value = r.Next(from, to);
+
+            } while (existingSet.Contains(value));
+
+            return value;
+        }
+            
+    }
+   public static class CommonFunc
+    {
+        public static void Exchange<T>(ref T leftValue, ref T rightValue)
+        {
+            T temp = rightValue;
+            rightValue = leftValue;
+            leftValue = temp;
+        }
+
+        public static int[] RandomizeArray(int[] inputArray)
+        {
+            RandowWithNoDuplication randomGeneratorWithNoDuplication = new RandowWithNoDuplication(0, inputArray.Length * 5);
+            var inputListWithPriority = inputArray.AsEnumerable()
+                .Select(ele=> new { value = ele, priority = randomGeneratorWithNoDuplication.GetRandomValue() });
+
+            return inputListWithPriority.AsEnumerable().OrderBy(ele => ele.priority).Select(ele=>ele.value).ToArray();    
+
+        }
+    }
+}
---
+++ CLRS/Chapter9/9.2/FindTheNthElementTest/FindTheNthElementTest/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace FindTheNthElementTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inputArrayTest = new int[100];
+            for (int i = 0; i < inputArrayTest.Length; i++)
+            {
+                inputArrayTest[i] = i;
+            }
+
+            int[] randomizedArray = CommonFunc.RandomizeArray(inputArrayTest);
+
+
+
+            Console.WriteLine(string.Join(",", randomizedArray));
+            Console.WriteLine(RandomSelect(randomizedArray, 0, randomizedArray.Length - 1, 50));
+        }
+
+        public static int RandomSelect(int[] inputArray, int startIndex, int endIndex, int ith)
+        {
+            if (startIndex == endIndex)
+            {
+                return inputArray[startIndex];
+            }
+
+            // 第i个元素总是相对于当前的startIndex
+            int pivotIndex = RandomPartition(inputArray, startIndex, endIndex);
+            int currentPivotOrder = pivotIndex - startIndex + 1;
+            if (currentPivotOrder == ith)
+            {
+                return inputArray[pivotIndex];
+            }
+
+            if (ith < currentPivotOrder)
+            {
+                return RandomSelect(inputArray, startIndex, pivotIndex - 1, ith);
+            }
+            else
+            {
+                return RandomSelect(inputArray, pivotIndex + 1, endIndex, ith - currentPivotOrder);
+            }
+        }
+
+        public static int RandomPartition(int[] inputArray, int startIndex, int endIndex)
+        {
+            int pivotValue = inputArray[endIndex];
+            int lessThanIndex = startIndex - 1;
+            for (int i = startIndex; i < endIndex - 1; i++)
+            {
+                if (inputArray[i] <= pivotValue)
+                {
+                    lessThanIndex++;
+                    CommonFunc.Exchange(ref inputArray[i], ref inputArray[lessThanIndex]);
+                }
+            }
+
+            CommonFunc.Exchange(ref inputArray[lessThanIndex + 1], ref inputArray[endIndex]);
+
+            return lessThanIndex + 1;
+        }
+
+
+
+
+    }
+}
