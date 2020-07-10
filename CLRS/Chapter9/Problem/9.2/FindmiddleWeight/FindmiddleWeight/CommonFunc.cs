d8121b7e8bd15f242793dcf68eecd519dd049891
Homer Guo <guochao6398@126.com>
2020-07-11 01:25:44 +0800

    思考题9-2 c

---
+++ CLRS/Chapter9/Problem/9.2/FindmiddleWeight/FindmiddleWeight/CommonFunc.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace CLRS.Common
+{
+
+    public class RandowWithNoDuplication
+    {
+
+        private HashSet<int> existingSet = new HashSet<int>();
+        private int from, to;
+        public RandowWithNoDuplication(int from, int end)
+        {
+            this.from = from;
+            this.to = end;
+        }
+
+        public int GetRandomValue()
+        {
+            Random r = new Random();
+            int value = 0;
+            do
+            {
+                value = r.Next(from, to);
+
+            } while (existingSet.Contains(value));
+
+            return value;
+        }
+            
+    }
+   public static class CommonFunc
+    {
+        public static void Exchange<T>(ref T leftValue, ref T rightValue)
+        {
+            T temp = rightValue;
+            rightValue = leftValue;
+            leftValue = temp;
+        }
+
+        public static T[] RandomizeArray<T>(T[] inputArray)
+        {
+            RandowWithNoDuplication randomGeneratorWithNoDuplication = new RandowWithNoDuplication(0, inputArray.Length * 5);
+            var inputListWithPriority = inputArray.AsEnumerable()
+                .Select(ele=> new { value = ele, priority = randomGeneratorWithNoDuplication.GetRandomValue() });
+
+            return inputListWithPriority.AsEnumerable().OrderBy(ele => ele.priority).Select(ele=>ele.value).ToArray();    
+
+        }
+
+        public static int RandomPartition<T>(T[] inputArray, int startIndex, int endIndex) 
+            where T : IComparable<T>
+        {
+            T pivotValue = inputArray[endIndex];
+            int lessThanIndex = startIndex - 1;
+            for (int i = startIndex; i <= endIndex - 1; i++)
+            {
+                if (inputArray[i].CompareTo(pivotValue) <= 0)
+                {
+                    CommonFunc.Exchange(ref inputArray[i], ref inputArray[++lessThanIndex]);
+                }
+            }
+
+            CommonFunc.Exchange(ref inputArray[lessThanIndex + 1], ref inputArray[endIndex]);
+
+            return lessThanIndex + 1;
+        }
+    }
+}
---
+++ CLRS/Chapter9/Problem/9.2/FindmiddleWeight/FindmiddleWeight/Program.cs
@@ -0,0 +1,79 @@
+﻿using CLRS.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindmiddleWeight
+{
+    public class ValueIndex
+    { 
+        public double Value
+        { get; set; }
+
+        public int Index
+        { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double[] inputArray = new double[] { 0.1,0.3,0.02, 0.03,0.05,0.1,0.07,0.08, 0.05,0.2};
+            if (inputArray.AsEnumerable().Sum() != 1)
+            {
+                throw new Exception("total length should be 1");
+            }
+            inputArray = CommonFunc.RandomizeArray(inputArray);
+            Console.WriteLine(inputArray[GetMiddleWeightIndex(inputArray, 0, inputArray.Length-1)]);
+            Console.WriteLine(string.Join(",", inputArray.AsEnumerable().OrderBy(e=>e)));
+        }
+
+        public static int GetMiddleWeightIndex(double[] inputArray, int startIndex, int endIndex)
+        {
+            int pivotIndex = CommonFunc.RandomPartition(inputArray, startIndex, endIndex);
+            double leftAmount, RightAmount;
+            if (pivotIndex == startIndex)
+            {
+                leftAmount = 0;
+            }
+            else
+            {
+                leftAmount = GetAmount(inputArray, startIndex, pivotIndex-1);
+            }
+
+            if (pivotIndex == endIndex)
+            {
+                RightAmount = 0;
+            }
+            else
+            {
+                RightAmount = GetAmount(inputArray, pivotIndex+1, inputArray.Length-1);
+            }
+
+            if (leftAmount < 0.5 && RightAmount <= 0.5)
+            {
+                return pivotIndex;
+            }
+            else if (leftAmount < 0.5)
+            {
+                return GetMiddleWeightIndex(inputArray, pivotIndex + 1, endIndex);
+            }
+            else
+            {
+                return GetMiddleWeightIndex(inputArray, startIndex, pivotIndex - 1);
+            }
+        }
+
+        private static double GetAmount(double[] inputArray, int sIndex, int eIndex)
+        {
+            double sumValue = 0;
+            for (int i = sIndex; i <= eIndex; i++)
+            {
+                sumValue += inputArray[i];
+            }
+            return sumValue;
+        }
+    }
+}
