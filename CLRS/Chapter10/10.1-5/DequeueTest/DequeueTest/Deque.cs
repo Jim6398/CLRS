ba07257e3991f74e2d1e384b6c02e7b1a71685cb
Homer Guo <guochao6398@126.com>
2020-07-16 19:37:49 +0800

    10.1-5 deque implementation, not fully tested.

---
+++ CLRS/Chapter10/10.1-5/DequeueTest/DequeueTest/Deque.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DequeueTest
+{
+    public class Deque<T>
+    {
+        const int defaultSize = 1024;
+        private T[] insideArray = new T[1024];
+        int head, tail;
+        int count;
+        public Deque(int size = defaultSize)
+        {
+            head  = 0; 
+            tail = 0;
+            count = 0;
+            insideArray = new T[size];
+        }
+
+        public int Count
+        {
+            get
+            {
+                return count;
+            }
+        }
+
+        public bool IsFull
+        {
+            get
+            { 
+                return this.insideArray.Length == count;
+            }           
+        }
+
+        public void TailEnqueue(T value)
+        {
+            if (IsFull)
+            {
+                Resize();
+            }
+
+            this.insideArray[tail++] = value;
+            if (tail == this.insideArray.Length)
+            {
+                tail = 0;
+            }
+
+            this.count++;
+        }
+
+        public void HeadEnqueue(T value)
+        {
+            if (IsFull)
+            {
+                Resize();
+            }
+
+            head--;
+            if (head < 0)
+            {
+                head = this.insideArray.Length - 1;
+            }
+
+            this.insideArray[head] = value;
+            this.count++;
+        }
+
+        public bool IsEmpty
+        {
+            get
+            {
+                return this.count == 0;
+            }
+        }
+
+        public T HeadDequeue()
+        {
+            if (this.count == 0)
+            {
+                throw new Exception("there is no element to dequeue");
+            }
+
+            T dequeueValue = this.insideArray[head++];
+            if (head == this.insideArray.Length)
+            {
+                head = 0;
+            }
+
+            this.count--;
+
+            return dequeueValue;
+        }
+
+        public T TailDequeue()
+        {
+            if (this.count == 0)
+            {
+                throw new Exception("there is no element to dequeue");
+            }
+
+            this.tail--;
+            if (this.tail < 0)
+            {
+                this.tail = this.insideArray.Length - 1;
+            }
+            this.count--;
+            return this.insideArray[this.tail];
+        }
+
+        private void Resize()
+        {
+            T[] oldArray = this.insideArray;
+            T[] newArray = new T[oldArray.Length * 2];
+            int i = head;
+            int newIndex = 0;
+            while (i != tail)
+            {
+                newArray[newIndex++] = oldArray[i++];
+                if (i == oldArray.Length)
+                {
+                    i = 0;
+                }             
+            }
+
+            this.head = 0;
+            this.tail = newIndex;
+            this.insideArray = newArray;
+        }
+
+        public IEnumerable<T> GetAllElement()
+        {
+            int i = this.head;       
+            do
+            {
+                T value = this.insideArray[i];
+                i++;
+                if (i == this.insideArray.Length)
+                {
+                    i = 0;
+                }
+                yield return value;
+            } while (i != tail) ;
+        }
+    }
+}
---
+++ CLRS/Chapter10/10.1-5/DequeueTest/DequeueTest/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DequeueTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Deque<int> dq = new Deque<int>(10);
+
+            for (int i = 0; i < 7; i++)
+            {
+                dq.HeadEnqueue(i);
+            }
+
+            for (int i = 0; i < 3; i++)
+            {
+                dq.TailDequeue();
+            }
+
+            for (int i = 7; i < 13; i++)
+            {
+                dq.TailEnqueue(i);
+            }
+
+            Console.WriteLine(string.Join(",", dq.GetAllElement()));
+        }
+    }
+}
