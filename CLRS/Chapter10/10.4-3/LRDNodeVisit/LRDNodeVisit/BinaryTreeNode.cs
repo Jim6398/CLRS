583d33fc9cc3da7f81ba75915848d2fa8dad4386
Homer Guo <guochao6398@126.com>
2020-08-02 13:42:37 +0800

    练习题10.4-3。后序遍历实现非递归的调用。省略前序和中序，因为这两个比较简单。

---
+++ CLRS/Chapter10/10.4-3/LRDNodeVisit/LRDNodeVisit/BinaryTreeNode.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LRDNodeVisit
+{
+    public class BinaryTreeNode<T>
+    {
+        public T KeyValue
+        {
+            get; set;
+        }
+
+        public BinaryTreeNode<T> LeftChild
+        { get; set; }
+        public BinaryTreeNode<T> RightChild
+        { get; set; }
+        public BinaryTreeNode<T> ParentNode
+        {get;set;}     
+    }
+
+    public class BinaryNodeTable<T>
+    {
+        public int currentNodeIndex
+        {
+            get;set;
+        }
+        public T key
+        {
+            get; set;
+        }
+
+        public int LeftChildIndex
+        {get;set;}
+
+        public int rightChildIndex
+        { get; set; }
+    }
+
+    public class BinaryTree<T>
+    {
+        class stackNode
+        {
+
+            public stackNode()
+                {
+                this.isRightChildPushed = false;
+            }
+            public BinaryTreeNode<T> node
+            { get; set; }
+
+            public bool isRightChildPushed
+            { get; set; }
+
+            public bool isLeftChildPushed
+            { get; set; }
+        }
+
+        public BinaryTreeNode<T> rootNode
+        { get; set; }
+
+        public BinaryTree(BinaryNodeTable<T>[] binaryNodeTableList, int rootIndex)
+        {
+            Dictionary<int, BinaryNodeTable<T>> nodesDict = binaryNodeTableList.AsEnumerable()
+                .ToDictionary(ele => ele.currentNodeIndex,ele=> ele);
+           this.rootNode = ConstructBinaryTreeFromTreeNode(nodesDict, rootIndex);
+        }
+
+        public BinaryTreeNode<T> ConstructBinaryTreeFromTreeNode(Dictionary<int, BinaryNodeTable<T>> nodesDict, int rootIndex)
+        {
+            return new BinaryTreeNode<T>
+            {
+                KeyValue = nodesDict[rootIndex].key
+                ,
+                LeftChild = nodesDict[rootIndex].LeftChildIndex == int.MaxValue? null : ConstructBinaryTreeFromTreeNode(nodesDict, nodesDict[rootIndex].LeftChildIndex)
+                ,
+                RightChild = nodesDict[rootIndex].rightChildIndex == int.MaxValue ? null : ConstructBinaryTreeFromTreeNode(nodesDict, nodesDict[rootIndex].rightChildIndex)
+            };
+        }
+   
+        /// <summary>
+        /// 这个算法完全模拟递归调用过程。
+        /// </summary>
+        /// <returns></returns>
+        public string LRDByAStack()
+        {
+            StringBuilder resultSB = new StringBuilder();
+            Stack<stackNode> stackUnvisited = new Stack<stackNode>();
+            stackUnvisited.Push(new stackNode { node = rootNode,isLeftChildVisited=false, isRightChildPushed = false });
+            while (stackUnvisited.Count() != 0)
+            {
+                var currentNode = stackUnvisited.Peek();
+
+                if (!currentNode.isLeftChildPushed)
+                {
+                    currentNode.isLeftChildPushed = true;
+                    if (currentNode.node.LeftChild == null)
+                    {
+                        continue;
+                    }
+                    stackUnvisited.Push(new stackNode { node = currentNode.node.LeftChild, isLeftChildPushed = false, isRightChildPushed = false });
+                }
+                else if (!currentNode.isRightChildPushed)
+                {
+                    currentNode.isRightChildPushed = true;
+                    if (currentNode.node.RightChild == null)
+                    {
+                        continue;
+                    }
+                    stackUnvisited.Push(new stackNode { node = currentNode.node.RightChild, isLeftChildPushed = false, isRightChildPushed = false });
+                }
+                else
+                {
+                    var node = stackUnvisited.Pop();
+                    resultSB.Append(node.node.KeyValue.ToString());
+                }
+            };
+            return resultSB.ToString();
+        }
+
+        public void LRDRecursive(BinaryTreeNode<T> node)
+        {
+            if (node == null)
+            {
+                return;
+            }
+            LRDRecursive(node.LeftChild);
+            LRDRecursive(node.RightChild);
+            Console.Write(node.KeyValue.ToString());
+             
+
+        }
+    }
+}
---
+++ CLRS/Chapter10/10.4-3/LRDNodeVisit/LRDNodeVisit/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LRDNodeVisit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BinaryNodeTable<int>[] table = new BinaryNodeTable<int>[]
+            {
+               new BinaryNodeTable<int>{ currentNodeIndex=1, key=12, LeftChildIndex=7, rightChildIndex=3 },
+               new BinaryNodeTable<int>{ currentNodeIndex=2, key=15, LeftChildIndex=8, rightChildIndex=int.MaxValue },
+               new BinaryNodeTable<int>{ currentNodeIndex=3, key=4, LeftChildIndex=10, rightChildIndex=int.MaxValue },
+               new BinaryNodeTable<int>{ currentNodeIndex=4,key=10, LeftChildIndex=5, rightChildIndex=9 },
+               new BinaryNodeTable<int>{ currentNodeIndex=5,key=2, LeftChildIndex=int.MaxValue, rightChildIndex=int.MaxValue},
+               new BinaryNodeTable<int>{ currentNodeIndex=6,key=18, LeftChildIndex=1, rightChildIndex=4 },
+               new BinaryNodeTable<int>{ currentNodeIndex=7,key=7, LeftChildIndex=int.MaxValue, rightChildIndex=int.MaxValue},
+               new BinaryNodeTable<int>{ currentNodeIndex=8,key=14, LeftChildIndex=6, rightChildIndex=2 },
+               new BinaryNodeTable<int>{ currentNodeIndex=9,key=21, LeftChildIndex=int.MaxValue, rightChildIndex=int.MaxValue },
+               new BinaryNodeTable<int>{ currentNodeIndex=10,key=5, LeftChildIndex=int.MaxValue, rightChildIndex=int.MaxValue }
+            };
+
+            BinaryTree<int> binaryTree = new BinaryTree<int>(table, 6);
+            Console.WriteLine(binaryTree.LRDByAStack().ToString());
+            binaryTree.LRDRecursive(binaryTree.rootNode);
+        }
+    }
+}
