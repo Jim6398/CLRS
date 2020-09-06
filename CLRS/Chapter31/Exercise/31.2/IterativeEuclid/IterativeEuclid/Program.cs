5df5b7572f1806bca9720f528442f32f04dc9959
Jim6398 <guochao6398@126.com>
2020-09-06 22:31:10 +0800

    31.2-4. Euclid with no recursivre.

---
+++ CLRS/Chapter31/Exercise/31.2/IterativeEuclid/IterativeEuclid/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IterativeEuclid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Euclid(899, 493));
+        }
+
+        public static int Euclid(int a, int b)
+        {
+            while (b != 0)
+            {
+                int temp = a % b;
+                a = b;
+                b = temp;
+            }
+
+            return a;
+        }
+    }
+}
