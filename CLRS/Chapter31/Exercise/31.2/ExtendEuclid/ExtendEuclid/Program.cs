1dda4a6f31ea570e1df0329792a3c5f276cf4a6a
Jim6398 <guochao6398@126.com>
2020-09-11 21:57:43 +0800

    ExtendEuclid.

---
+++ CLRS/Chapter31/Exercise/31.2/ExtendEuclid/ExtendEuclid/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtendEuclid
+{
+    public class EuclidEntity
+    {
+        public int D
+        { get; set;}
+        public int X
+        { get; set; }
+        public int Y
+        { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EuclidEntity euclidEntity = ExtendEuclid(99, 78);
+            Console.WriteLine($"{euclidEntity.X}*99 + {euclidEntity.Y}*78 = {euclidEntity.D}");
+        }
+
+        public static EuclidEntity ExtendEuclid(int a, int b)
+        {
+            if (b == 0)
+            {
+                return new EuclidEntity { D = a, X = 1, Y = 0 };
+            }
+
+            EuclidEntity preEuclidEntity = ExtendEuclid(b, a % b);
+
+            return new EuclidEntity { X=preEuclidEntity.Y, D = preEuclidEntity.D, Y = preEuclidEntity.X - (a/b) * preEuclidEntity.Y };
+        }
+    }
+}
